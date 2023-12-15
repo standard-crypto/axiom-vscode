2dae47e981ebcb12f4006908c51226a6730dff39
Kevin Ayling <kra4ck@virginia.edu>
2023-12-15 10:13:14 -0800

    fix relative pathing

--- src/utils.ts
+++ src/utils.ts
@@ -5,26 +5,49 @@ import { Query } from './models/query';
 
 export function setBuildPathForCircuit(circuit: Circuit): void {
     let buildPathPrefix:string = vscode.workspace.getConfiguration().get('axiomOutputFolder') ?? '';
+    let buildPath: string;
 
-    if (buildPathPrefix.length === 0) {
-        return;
+    if (!buildPathPrefix.startsWith('/')) {
+        buildPath = getCwd();
+        if (!buildPath.endsWith('/')) {
+            buildPath += '/';
+        }
+        buildPath += buildPathPrefix;
+    } else {
+        buildPath = buildPathPrefix;
     }
 
-    if (!buildPathPrefix.endsWith('/')) {
-        buildPathPrefix += '/';
+    if (!buildPath.endsWith('/')) {
+        buildPath += '/';
     }
-    circuit.buildPath = vscode.Uri.parse(buildPathPrefix  + 'build.json');
+    circuit.buildPath = vscode.Uri.parse(buildPath  + 'build.json');
 }
 
 export function setOutputPathForQuery(query: Query): void {
     let outputPathPrefix:string = vscode.workspace.getConfiguration().get('axiomOutputFolder') ?? '';
+    let outputPath:string;
 
-    if (outputPathPrefix.length === 0) {
-        return;
+    if (!outputPathPrefix.startsWith('/')) {
+        outputPath = getCwd();
+        if (!outputPath.endsWith('/')) {
+            outputPath += '/';
+        }
+        outputPath += outputPathPrefix;
+    } else {
+        outputPath = outputPathPrefix;
     }
 
-    if (!outputPathPrefix.endsWith('/')) {
-        outputPathPrefix += '/';
+    if (!outputPath.endsWith('/')) {
+        outputPath += '/';
     }
-    query.outputPath = vscode.Uri.parse(outputPathPrefix  + 'output.json');
+    query.outputPath = vscode.Uri.parse(outputPath  + 'output.json');
+}
+
+function getCwd(): string {
+    const workspaces = vscode.workspace.workspaceFolders;
+    if (workspaces !== undefined) {
+        const cwdUri = workspaces[0].uri.toString();
+        return cwdUri.replace('file://', '');
+    }
+    return '';
 }
